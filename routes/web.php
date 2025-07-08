3384d67ab79f47454bb686fe25b76928ce2dcdae
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-07-09 01:21:24 +0600

    feat: web route added

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', function () {
 
 Route::resource('category', CategoryController::class);
 Route::resource('attribute', AttributeController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
